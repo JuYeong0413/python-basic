0b2b8c342078d6014d27c95b1c1081ebbbec6b0f
Juyeong Lee <2jy22@naver.com>
2021-01-28 03:25:52 +0900

    파이썬 Print 사용법(1-1)

---
+++ chapter02_01.py
@@ -0,0 +1,19 @@
+# Chapter02-1
+# 파이썬 완전 기초
+# Print 사용법
+# 참조 : https://www.python-course.eu/python3_formatted_output.php
+
+# 기본 출력
+## 보통 작은따옴표를 많이 사용함
+print('Python Start!')
+print("Python Start!")
+print('''Python Start!''')
+print("""Python Start!""")
+
+print()
+
+# separator 옵션
+## => 분리가 무엇으로 되어있는지?
+print('P', 'Y', 'T', 'H', 'O', 'N', sep='')
+print('010', '7777', '1234', sep='-') ## 파라미터 결합을 '-'으로 시켜준다.
+print('python', 'google.com', sep='@')
