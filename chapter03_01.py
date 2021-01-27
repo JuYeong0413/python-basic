49eb536e72f7b4c16544b59ab2c5527adfa7c112
Juyeong Lee <2jy22@naver.com>
2021-01-28 04:57:45 +0900

    숫자형(1-2)

--- chapter03_01.py
+++ chapter03_01.py
@@ -18,8 +18,8 @@ dict : 사전
 str1 = "Python"
 bool = True
 str2 = 'Anaconda'
-float = 10.0 # 10 != 10.0
-int = 7
+float_v = 10.0 # 10 != 10.0
+int_v = 7
 list = [str1, str2]
 dict = { ## key: value
     "name": "Machine Learning",
@@ -33,8 +33,8 @@ set = {3, 5, 7}
 print(type(str1))
 print(type(bool))
 print(type(str2))
-print(type(float))
-print(type(int))
+print(type(float_v))
+print(type(int_v))
 print(type(list))
 print(type(dict))
 print(type(tuple))
@@ -105,3 +105,36 @@ print(">>>>>/")
 print("i1 / i2 : ", i1 / i2)
 print("f1 / f2 : ", f1 / f2)
 print("big_int1 / big_int2 : ", big_int1 / big_int2)
+
+# 형 변환 실습
+a = 3. ## 3.0
+b = 6
+c = .7 ##0.7
+d = 12.7
+
+# 타입 출력
+print(type(a), type(b), type(c), type(d))
+
+# 형 변환
+print(float(b))
+print(int(c))
+print(int(d)) ## .7 절삭
+print(int(True)) # True : 1, False : 0
+print(float(False))
+print(complex(3))
+print(complex('3')) # 문자형 -> 숫자형
+print(complex(False))
+print()
+
+# 수치 연산 함수
+print(abs(-7))
+x, y = divmod(100, 8) ## x : 몫, y : 나머지
+
+print(x,y)
+print(pow(5, 3), 5 ** 3)
+
+# 외부 모듈
+import math
+
+print(math.ceil(5.1)) # x 이상의 수 중에서 가장 작은 정수
+print(math.pi)
