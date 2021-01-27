3a428fb3d709cff92036d9c1562e58b570470272
Juyeong Lee <2jy22@naver.com>
2021-01-28 04:28:48 +0900

    파이썬 변수(2-2)

--- chapter02_02.py
+++ chapter02_02.py
@@ -52,3 +52,52 @@ m = 400
 print(m, n)
 print(type(m), type(n))
 print()
+
+# id(identity) 확인 : 객체의 고유값 확인
+m = 800
+n = 655
+
+print(id(m)) ## 오브젝트의 고유값
+print(id(n))
+print(id(m) == id(n))
+print()
+
+# 같은 오브젝트 참조
+## 4개를 선언했지만 1개만 존재함
+a = 800
+b = 800
+c = 800
+d = 800
+print(id(a))
+print(id(b))
+print(id(a) == id(b))
+print()
+
+# 다양한 변수 선언
+# Camel Case : numberOfCollegeGraduates -> Method
+# Pascal Case : NumberOfCollegeGraduates -> Class
+# Snake Case : number_of_college_graduates
+
+# 허용하는 변수 선언법
+age = 1
+Age = 2
+aGe = 3
+AGE = 4
+a_g_e = 5
+_age = 6
+age_ = 7
+_AGE_ = 8
+
+## 특수문자나 숫자로 시작하는 건 허용되지 않음
+# 예약어는 변수명으로 불가능
+"""
+False   def if  raise
+None    del import  return
+True    elif    in  try
+and else    is  while
+as  except  lambda  with
+assert  finally nonlocal    yield
+break   for  not
+class   from    or
+continue    global  pass
+"""
