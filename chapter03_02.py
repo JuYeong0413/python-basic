cd4ea84bb9678e671b464554328cf6baf09786c7
Juyeong Lee <2jy22@naver.com>
2021-01-28 19:35:42 +0900

    문자형(2-2)

--- chapter03_02.py
+++ chapter03_02.py
@@ -56,3 +56,48 @@ print()
 raw_s = r'D:\python\test' ## 있는 그대로 표시
 print(raw_s)
 print()
+
+# 멀티라인 입력
+# 역슬래시 사용
+multi_str = \
+'''
+String
+Multi line
+Test
+'''
+
+print(multi_str)
+
+# 문자열 연산
+str_o1 = "python"
+str_o2 = "Apple"
+str_o3 = "How are you doing"
+str_o4 = "Seoul!Daejeon!Busan!Jinju"
+
+print(str_o1 * 3)
+print(str_o1 + str_o2)
+print('y' in str_o1)
+print('n' in str_o1)
+print('P' not in str_o2)
+
+# 문자열 형 변환
+print(str(66), type(str(66)))
+print(str(10.1))
+print(str(True), type(str(True)))
+
+# 문자열 함수(upper, isalnum, startswith, count, endswith, isalpha...)
+
+
+print("Capitalize: ", str_o1.capitalize()) ## 첫 번째 글자를 대문자로 바꿔준다.
+print("endswith?: ", str_o2.endswith("!"))
+print("replace", str_o1.replace("thon", ' Good'))
+print("sorted: ", sorted(str_o1)) ## 정렬해서 리스트 형태로 반환
+print("split: ", str_o4.split('!'))
+
+# 반복(시퀀스)
+im_str = "Good Boy!"
+print(dir(im_str)) # __iter__
+
+# 출력
+for i in im_str:
+    print(i)
