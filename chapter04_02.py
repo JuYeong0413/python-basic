08888e9bf643b19134159667788444dec0399ece
Juyeong Lee <2jy22@naver.com>
2021-01-29 03:46:17 +0900

    for 구문(2-2)

--- chapter04_02.py
+++ chapter04_02.py
@@ -38,3 +38,52 @@ names = ['Kim', 'Park', 'Cho', 'Lee', 'Choi', 'Yoo']
 
 for n in names:
     print('You are : ', n)
+
+# 예제2
+lotto_numbers = [11, 19, 21, 28, 36, 37]
+for n in lotto_numbers:
+    print("Current number : ", n)
+
+# 예제3
+word = "Beautiful"
+for s in word:
+    print('word : ', s)
+
+# 예제4
+my_info = {
+    "name": 'Lee',
+    "Age": 33,
+    "City": "Seoul"
+}
+for key in my_info:
+    print('key :', my_info[key])
+    # print('key : ', my_info.get(key))
+
+for v in my_info.values():
+    print('value :', v)
+
+# 예제5
+name = 'PineAppLE'
+for n in name:
+    if n.isupper:
+        print(n)
+    else:
+        print(n.upper())
+
+# break
+numbers = [14, 3, 4, 7, 10, 24, 17, 2, 33, 15, 34, 36, 38]
+for num in numbers:
+    if num == 34:
+        print('Found : 34!')
+        break ## break를 만나면 빠져나간다.
+    else:
+        print('Not found : ', num)
+
+# continue
+lt = ["1", 2, 5, True, 4.3, complex(4)]
+for v in lt:
+    if type(v) is bool: ## 자료형 대조는 is, is not
+        continue ## 다시 조건을 검사하는 부분으로 이동함
+    print("current type:", v, type(v))
+    print("multiply by 2", v * 2)
+    # print(True * 3)
