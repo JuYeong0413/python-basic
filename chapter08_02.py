ad7549731b9c61382ecda0384a5424ad061bf6e7
Juyeong Lee <2jy22@naver.com>
2021-01-30 04:14:16 +0900

    External Function(2-2)

--- chapter08_02.py
+++ chapter08_02.py
@@ -27,3 +27,59 @@ f = open('test.obj', 'rb') ## read binary
 data = pickle.load(f)
 print(data, type(data))
 f.close()
+
+# os : 환경 변수, 디렉토리(파일) 처리 관련, 운영체제 작업 관련
+# mkdir, rmdir(비어 있으면 삭제), rename
+
+# 예제6
+import os
+print(os.environ)
+print(os.environ["USER"])
+print(os.environ["ATOM_HOME"])
+
+# 예제7 (현재 경로)
+print(os.getcwd())
+
+# time : 시간 관련 처리
+import time
+
+# 예제8
+print(time.time())
+
+# 예제9 (형태 변환)
+print(time.localtime(time.time())) ## 현재 시간을 모두 분해해서 보여줌
+
+# 예제10 (간단 표현)
+print(time.ctime())
+
+# 예제11 (형식 표현)
+print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))) ## string format time
+
+# 예제12 (시간 간격 발생)
+for i in range(5):
+    print(i)
+    time.sleep(1) ## 1초간 쉰다.
+
+# random : 난수 리턴
+import random
+
+# 예제13
+print(random.random()) # 0 ~ 1 실수
+
+# 예제14
+print(random.randint(1, 45)) ## 1 ~ 45
+print(random.randrange(1, 45)) ## 1 ~ 44
+
+# 예제15 (섞기) shuffle
+d = [1, 2, 3, 4, 5]
+random.shuffle(d)
+print(d)
+
+# 예제16 (무작위 선택)
+c = random.choice(d) ## 하나 선택
+print(c)
+
+# webbrowser : 본인 OS의 웹 브라우저 실행
+import webbrowser
+webbrowser.open("http://google.com")
+webbrowser.open_new("http://google.com")
