fbb1842a44f0caa71e331e246b2d8f0dce4cd3a1
Juyeong Lee <2jy22@naver.com>
2021-01-29 19:34:48 +0900

    Module(2-2)

--- module_test.py
+++ module_test.py
@@ -3,3 +3,17 @@
 import sys
 
 print(sys.path)
+
+print(type(sys.path))
+
+# 모듈 경로 삽입
+sys.path.append('/Users/juyeong/django_workspace/math')
+
+print(sys.path)
+
+import test_module
+
+# 모듈 사용
+print(test_module.power(10, 3))
+
+import chapter06_02
